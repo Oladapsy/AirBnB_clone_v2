6f92e898c132ceeda3370925f615fe2868cb1a49
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-10 16:27:29 +0100

    test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,11 +3,10 @@
 
 sudo apt-get update -y
 sudo apt-get install -y nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
+
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
 echo "<html>
   <head>
     </head>
@@ -15,18 +14,30 @@ echo "<html>
           Holberton School
 	    </body>
 	    </html>" > /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
-ln -s /data/web_static/current /data/web_static/releases/test/
-sudo chown -R uubuntu:ubuntu /data/
 
-#update Nginx configuration
-echo "server {
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 
-	location /hbnb_static {
-		alias /data/web_static/current/;
-	}
+sudo printf %s "server {
+    listen 80;
+    listen [::]:80 default_server;
 
-}" >> /etc/nginx/sites-available/default
+    add_header X-Served-By $HOSTNAME;
+
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
 sudo nginx -s reload
 sudo service nginx restart
