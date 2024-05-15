62f66f7d0a714a1d76990b8da39516ea203bf5f6
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-15 17:05:58 +0100

    debuging

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -39,7 +39,6 @@ def do_deploy(archive_path):
             format(name)).failed is True:
         return False
 
-
     if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{} /data/web_static/current".
