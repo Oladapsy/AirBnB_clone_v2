b5a95eb3111ff4d1a8180cbdbd9e9ee141b08676
Oladapsy <dapoodedeyi@yahoo.com>
2024-05-15 15:48:55 +0100

    retry1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,22 +22,18 @@ def do_deploy(archive_path):
         return False
     if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
         return False
-    result = run("mkdir -p /data/web_static/releases/{}".format(name))
-    if result.failed:
+    if run("mkdir -p /data/web_static/releases/{}".
+            format(name)).failed is True:
         return False
 
-    result = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
-                 format(file, name))
-    if result.failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
+            format(file, name)).failed is True:
         return False
-    result = run("rm /tmp/{}".format(file))
-    if result.failed:
+    if run("rm /tmp/{}".format(file)).failed is True:
         return False
-    result = run("rm -rf /data/web_static/current")
-    if result.failed:
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
-    result = run("ln -s /data/web_static/releases/{} /data/web_static/current".
-                 format(name))
-    if result.failed:
+    if run("ln -s /data/web_static/releases/{} /data/web_static/current".
+            format(name)).failed is True:
         return False
     return True
